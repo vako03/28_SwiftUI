8f8e5c5f5c44e40f6a68c39c47580e024c7259c9
Vako <valeri.mekhashishvili97@gmail.com>
2024-05-22 10:16:02 +0400

    Initial Commit

---
+++ 28_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  28_SwiftUI
+//
+//  Created by valeri mekhashishvili on 22.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 28_SwiftUI/_8_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8_SwiftUIApp.swift
+//  28_SwiftUI
+//
+//  Created by valeri mekhashishvili on 22.05.24.
+//
+
+import SwiftUI
+
+@main
+struct _8_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
